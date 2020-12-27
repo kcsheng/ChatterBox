ae9fc4d43f6bf781619cc70dedc80a3936f6c029
kcsheng <kacysheng@mail.com>
2020-12-27 12:31:04 +1100

    Add email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,7 @@ class User < ApplicationRecord
   end
 
   def activate
-    self.update_attribute(:activated, true)
-    self.update_attribute(:activated_at, Time.zone.now)
+    self.update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   def send_activation_email
